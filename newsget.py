d5cf1e720897376e3957ad533646c616750040a4
Daryl Lang <daryl@daryllang.com>
2016-05-02 06:25:04 -0400

    Fixes OK message in newsget

--- newsget.py
+++ newsget.py
@@ -6,7 +6,5 @@ f = open(config.datapath + '/news.xml','w')
 response = urllib2.urlopen('http://feeds.abcnews.com/abcnews/topstories')
 xmlfile = response.read()
 
-print "OK"
-
 f.write(xmlfile)
 f.close()
